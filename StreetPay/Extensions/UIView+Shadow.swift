b128869f1a40ad4807285f51f0c18733b54e83c6
Bruno Barbosa <bbt@cin.ufpe.br>
2019-02-09 21:04:03 -0300

    Created UIView extension to add shadowing

---
+++ StreetPay/Extensions/UIView+Shadow.swift
@@ -0,0 +1,18 @@
+//
+//  UIView+Shadow.swift
+//  StreetPay
+//
+//  Created by Bruno Barbosa on 09/02/19.
+//  Copyright © 2019 Bruno Barbosa. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    func addDropShadow(withRadius radius: CGFloat = 8.0, andOffset offset: CGSize = CGSize(width: 0, height: 2)) {
+        self.layer.shadowColor = UIColor.black.cgColor
+        self.layer.shadowOffset = offset
+        self.layer.shadowOpacity = 0.5
+        self.layer.shadowRadius = radius
+    }
+}
